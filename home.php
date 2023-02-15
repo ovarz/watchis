3e41347ab20377dda159dd8e10969a5a43ec3fa7
ovarz <ovarz_03@yahoo.com>
2023-02-15 10:29:12 +0700

    1

--- home.php
+++ home.php
@@ -8,7 +8,26 @@
 <?php require ($_SERVER['WATCHIS'].'inc/sample.php')?>
 <?php require ($_SERVER['WATCHIS'].'inc/meta.php')?>
 <div class="rancak-container">
-  tes
+  <div class="header-home">
+    <div class="header-home-bg flex_ori thumb-loading">
+	  <img alt="img_title" class="lazyload" data-original="img/home-header.png" />
+	</div>
+	<div class="header-home-content">
+	  <div class="hhc-top flex_ori thumb-loading">
+	    <img alt="img_title" class="lazyload" data-original="img/logo-icon.png" />
+	  </div>
+	  <div class="hhc-bottom">
+	    <div class="hhc-bottom-time">Subuh 04:36 WIB</div>
+	    <div class="hhc-bottom-date">2 Februari 2023 / 11 Rajab 1444</div>
+	  </div>
+	</div>
+  </div>
+  
+  
+  
+  <div class="article-list">
+    
+  </div>
 </div>
 <?php require ($_SERVER['WATCHIS'].'inc/footer.php')?>
 <?php require ($_SERVER['WATCHIS'].'inc/menu.php')?>
