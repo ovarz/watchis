d3b3faf8ac511b6f4794c38659138e612484316d
ovarz <ovarz_03@yahoo.com>
2023-03-28 17:35:14 +0700

    Update detail-article.php

--- detail-article.php
+++ detail-article.php
@@ -18,6 +18,17 @@
 	<?php require ($_SERVER['WATCHIS'].'module/detail-head.php')?>
 	<?php require ($_SERVER['WATCHIS'].'module/detail-content.php')?>
   </main>
+  
+  
+  
+  <section class="section-container section-article">
+    <div class="section-title">Artikel Terkait</div>
+	<div class="section-article-list">
+	  <?php for ($i=1; $i <= 10 ; $i++) { ?>
+	    <?php $content_type='article'; $detail_link='detail-article'; require ($_SERVER['WATCHIS'].'module/content-list.php')?>
+	  <?php } ?>
+	</div>
+  </section>
 </div>
 <?php $share='yes'; require ($_SERVER['WATCHIS'].'inc/menu.php')?>
 <?php require ($_SERVER['WATCHIS'].'inc/base-bottom.php')?>
